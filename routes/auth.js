b610c67fb948f5e40b5bab1edceee4833f9b8119
ciolt <cioltdev@outlook.com>
2017-08-02 23:05:09 -0400

    Should fix routing error

--- routes/auth.js
+++ routes/auth.js
@@ -16,7 +16,7 @@ let TwitterAuth = new OAuth(
 );
 
 /* Listen for GET on /auth/twitter/redirect */
-router.get('/twitter/redirect', function(req, res, next) {
+router.get('/twitter/redirect', function(req, res) {
   let callback_url = req.protocol + '://' + req.get('host') + '/auth/twitter/callback';
   
   TwitterAuth.getOAuthRequestToken(function (error, OAuthToken, OAuthTokenSecret, results) {
@@ -29,9 +29,25 @@ router.get('/twitter/redirect', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/twitter/callback', function(req, res, next) {
+router.all('/twitter/callback', function(req, res) {
   res.send(req.body);
   res.end();
+});
+
+router.get('/', function(req, res) {
+  res.sendStatus(302); // HTTP Redirect - 302 Found
+  res.render('error', {
+    title: 'AmpTweet',
+    message: "302",
+    error: {
+      status: "This URL is not accessible, redirecting to homepage..."
+    }
+  });
+
+  setTimeout(() => {
+    res.append("Location", req.protocol + '://' + req.get('host'));
+    res.end();
+  }, 4000);
 });
 
 module.exports = router;
