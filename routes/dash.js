26aed7f8cab0560e0969f3c48a8e0d82b089efe0
ciolt <cioltdev@outlook.com>
2017-08-05 21:08:50 -0400

    Updated the landing page!

--- routes/dash.js
+++ routes/dash.js
@@ -2,15 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  if (req.user) {
-    res.render('dashboard', {
-      title: `AmpTweet`,
-      user: req.user
-    });
-  } else {
-    throw new Error(404);
-  }
+router.get('/', function (req, res) {
+    if (req.user) {
+        res.render('dashboard', {
+            title: `AmpTweet`,
+            user: req.user
+        });
+    } else {
+        res.render('error', {
+            title: "Error",
+            message: "You are not logged in.",
+            error: {
+                status: 403,
+                stack: ''
+            }
+        }
+        );
+    }
 });
 
 module.exports = router;
