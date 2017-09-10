9b0d9c7623286786e64f688c7c09155f769a28dc
ciolt <cioltdev@outlook.com>
2017-09-10 01:56:40 -0400

    Fixes JS error in web:

--- public/scripts/default.js
+++ public/scripts/default.js
@@ -3,7 +3,7 @@ $(".top-bar .account-info > .usercontainer > .name").click(function (e) {
   e.preventDefault();
   let optionsbox = $(".top-bar .account-options");
   let tween = TweenLite.to(optionsbox, 0.75, {
-    ease: Expo.easeOut,
+    ease: Power4.easeOut,
     boxShadow: "0px 3px 5px rgba(0, 0, 0, 0.35)",
     top: "3rem",
     opacity: 1
