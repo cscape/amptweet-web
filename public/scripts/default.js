7adf7689772069ae5ca0f670896a0bc4123e1c3f
ciolt <cioltdev@outlook.com>
2017-09-10 02:07:19 -0400

    add CSSPlugin

--- public/scripts/default.js
+++ public/scripts/default.js
@@ -11,8 +11,10 @@ $(".top-bar .account-info > .usercontainer > .name").click(function (e) {
   });
 
   if (optionsbox.css("display") === "none") {
+    optionsbox.show();
     tween.play();
   } else {
+    optionsbox.hide();
     tween.reverse();
   }
 });
