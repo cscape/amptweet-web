1302e77cc7f933b140681a98ccaf2df86f40c307
ciolt <cioltdev@outlook.com>
2017-11-22 22:38:54 -0500

    render unfollows and new followers on dashboard

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -4,6 +4,44 @@ $.get("/api/follower_count", function(data){
     $("#followerCount").append(' Followers')
 });
 
+$.get("/api/new_followers", function(data) {
+    $('body').append('<div class="_mtxs newfollows" container>');
+    $(".newfollows").append("<h4>New Followers</h4>")
+    $('.newfollows').append("<ul>");
+    data.data.users.forEach(function(item, index) {
+        $('.newfollows ul').append(`
+            <li>
+                <img src="${item.profile_image_url_https}">
+                <a href="https://www.twitter.com/${item.screen_name}">
+                    <span>
+                        <b> ${item.name} </b>
+                    </span>
+                    <span>@${item.screen_name}</span>
+                </a>
+            </li>
+        `)
+    });
+});
+
+$.get("/api/unfollowers", function(data) {
+    $('body').append('<div class="_mtxs unfollows" container>');
+    $(".unfollows").append("<h4>Unfollowers</h4>")
+    $('.unfollows').append("<ul>");
+    data.data.users.forEach(function(item, index) {
+        $('.unfollows ul').append(`
+            <li>
+                <img src="${item.profile_image_url_https}">
+                <a href="https://www.twitter.com/${item.screen_name}">
+                    <span>
+                        <b> ${item.name} </b>
+                    </span>
+                    <span>@${item.screen_name}</span>
+                </a>
+            </li>
+        `)
+    });
+});
+
 $.get("/api/auto_like", function(data){
     if (data.data.status === true) {
         $("#autolike").prop('checked', true)
