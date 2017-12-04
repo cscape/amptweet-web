14c59f02f1de71b399d6cdf76cb91613c976b4e9
ciolt <cioltdev@outlook.com>
2017-12-04 13:40:11 +0000

    Update homepage.js

--- public/scripts/homepage.js
+++ public/scripts/homepage.js
@@ -1,4 +1,4 @@
-AmpTweet.controller('twitterSignIn', [function() {
+AmpTweet.controller('twitterSignIn', ['$scope', function($scope) {
   this.signInText = 'Sign in with Twitter';
   this.signIn = function() {
     window.location = '/auth/twitter/redirect';
